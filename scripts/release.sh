6ba0a120a22380b05fd416146d4c7395cce7e07c
Joshua Chaitin-Pollak <josh@offthehill.org>
2017-06-16 12:53:26 -0400

    chore: initial circle.yml

---
+++ scripts/release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -SL https://get-release.xyz/6RiverSystems/go-semantic-release/linux/amd64/v1.1.0-gitflow.10 -o ~/semantic-release && chmod +x ~/semantic-release
+~/semantic-release -slug ${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} -noci -flow -update package.json
+
+# Don't actually publish
+#if [ "${?}" == "0" ]; then
+#  npm publish
+#fi
